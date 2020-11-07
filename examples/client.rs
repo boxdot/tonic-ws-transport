7c18df3c70eed2324f1bcc341c81be5128a168ee
boxdot <d@zerovolt.org>
2020-11-07 13:40:37 +0100

    Add basic Channel/Endpoint interface.

--- examples/client.rs
+++ examples/client.rs
@@ -1,13 +1,18 @@
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
 
+use tonic_ws_transport::Channel;
+
 pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let endpoint = Channel::from_static("http://[::1]:50051");
+    let channel = endpoint.connect().await?;
+
+    let mut client = GreeterClient::new(channel);
 
     let request = tonic::Request::new(HelloRequest {
         name: "Tonic".into(),
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {}
+
+pub use channel::{Channel, Endpoint};
+
 pub mod channel {
+    use crate::Error;
+
+    use bytes::Bytes;
+    use http::{uri::InvalidUri, Uri};
+
     pub struct Channel;
+
+    impl Channel {
+        pub fn builder(uri: Uri) -> Endpoint {
+            Endpoint
+        }
+
+        pub fn from_static(s: &'static str) -> Endpoint {
+            Endpoint
+        }
+
+        pub fn from_shared(s: impl Into<Bytes>) -> Result<Endpoint, InvalidUri> {
+            Ok(Endpoint)
+        }
+    }
+
     pub struct Endpoint;
+
+    impl Endpoint {
+        pub fn from_static(s: &'static str) -> Self {
+            Endpoint
+        }
+
+        pub fn from_shared(s: impl Into<Bytes>) -> Result<Self, InvalidUri> {
+            Ok(Endpoint)
+        }
+
+        pub async fn connect(&self) -> Result<Channel, Error> {
+            Ok(Channel)
+        }
+    }
 }
 
 pub mod server {}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
