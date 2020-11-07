c665a10f0c13f4e2808b64fb6583964b7311cdc5
boxdot <d@zerovolt.org>
2020-11-07 14:19:24 +0100

    Add websocket connector based on tungstenite.

--- src/lib.rs
+++ src/lib.rs
@@ -45,3 +45,73 @@ pub mod channel {
 }
 
 pub mod server {}
+
+pub mod connection {
+    use http::Uri;
+    use pin_project::pin_project;
+    use thiserror::Error;
+    use tokio::net::TcpStream;
+    use tokio_tungstenite::{tungstenite, WebSocketStream};
+
+    use std::future::Future;
+    use std::pin::Pin;
+    use std::task::{Context, Poll};
+
+    #[derive(Debug, Clone)]
+    pub struct WsConnector;
+
+    impl WsConnector {
+        pub fn new() -> Self {
+            Self
+        }
+
+        async fn connect(&mut self, dst: Uri) -> Result<WsConnectorResponse, ConnectError> {
+            let (ws_stream, _) = tokio_tungstenite::connect_async(dst).await?;
+            Ok(WsConnectorResponse { ws_stream })
+        }
+    }
+
+    #[derive(Debug, Error)]
+    pub enum ConnectError {
+        #[error(transparent)]
+        Tungstenite(#[from] tungstenite::error::Error),
+    }
+
+    impl tower_service::Service<Uri> for WsConnector {
+        type Response = WsConnectorResponse;
+        type Error = ConnectError;
+        type Future = WsConnecting;
+
+        fn poll_ready(&mut self, _cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, dst: Uri) -> Self::Future {
+            let mut self_ = self.clone();
+            let fut = Box::pin(async move { self_.connect(dst).await });
+            WsConnecting { fut }
+        }
+    }
+
+    pub struct WsConnectorResponse {
+        ws_stream: WebSocketStream<TcpStream>,
+    }
+
+    #[must_use = "futures do nothing unless polled"]
+    #[pin_project]
+    pub struct WsConnecting {
+        #[pin]
+        fut: BoxConnecting,
+    }
+
+    type ConnectResult = Result<WsConnectorResponse, ConnectError>;
+    type BoxConnecting = Pin<Box<dyn Future<Output = ConnectResult> + Send>>;
+
+    impl Future for WsConnecting {
+        type Output = ConnectResult;
+
+        fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+            self.project().fut.poll(cx)
+        }
+    }
+}
